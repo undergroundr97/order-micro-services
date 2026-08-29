e4c9a21bef8136e612e4991828aeb0bffdac0999
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-08-29 16:15:40 -0300

    feat: adiciona paymentsetvice

---
+++ paymentservice/src/main/java/com/miniordersystem/paymentservice/service/PaymentService.java
@@ -0,0 +1,16 @@
+package com.miniordersystem.paymentservice.service;
+
+import com.miniordersystem.paymentservice.repository.PaymentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+
+}
