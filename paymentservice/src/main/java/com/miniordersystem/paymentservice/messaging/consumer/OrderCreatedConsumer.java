ea7b7bdf94ac17b8b1f66639377487e73fb9842c
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-08-29 16:31:36 -0300

    feat: adiciona persistencia ao payment

--- paymentservice/src/main/java/com/miniordersystem/paymentservice/messaging/consumer/OrderCreatedConsumer.java
+++ paymentservice/src/main/java/com/miniordersystem/paymentservice/messaging/consumer/OrderCreatedConsumer.java
@@ -1,6 +1,8 @@
 package com.miniordersystem.paymentservice.messaging.consumer;
 
+import com.miniordersystem.paymentservice.PaymentserviceApplication;
 import com.miniordersystem.paymentservice.messaging.event.OrderCreatedEvent;
+import com.miniordersystem.paymentservice.service.PaymentService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +11,20 @@ import org.springframework.stereotype.Component;
 public class OrderCreatedConsumer {
 
 
+    private final PaymentService paymentService;
+
+
+    public OrderCreatedConsumer(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
     @RabbitListener(
             queues = "order.created.payment.queue"
     )
     public void consume(OrderCreatedEvent event){
 
-        System.out.println("Event: " + event);
+        System.out.println(event);
+        paymentService.processPayment(event);
     }
 
 
--- paymentservice/src/main/java/com/miniordersystem/paymentservice/service/PaymentService.java
+++ paymentservice/src/main/java/com/miniordersystem/paymentservice/service/PaymentService.java
@@ -1,7 +1,13 @@
 package com.miniordersystem.paymentservice.service;
 
+import com.miniordersystem.paymentservice.domain.Payment;
+import com.miniordersystem.paymentservice.domain.PaymentStatus;
+import com.miniordersystem.paymentservice.messaging.event.OrderCreatedEvent;
 import com.miniordersystem.paymentservice.repository.PaymentRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class PaymentService {
@@ -10,6 +16,19 @@ public class PaymentService {
 
     public PaymentService(PaymentRepository paymentRepository) {
         this.paymentRepository = paymentRepository;
+    }
+
+    public void processPayment(OrderCreatedEvent order){
+        Payment payment = new Payment(
+                null,
+                order.orderId(),
+                order.total(),
+                PaymentStatus.PENDING,
+                LocalDateTime.now(),
+                null
+        );
+
+        paymentRepository.save(payment);
     }
 
 
